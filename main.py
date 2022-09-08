625fe36d70b516b6af5716a53ab3bae7ae6d4884
hyper-glitch <graff049@gmail.com>
2022-09-08 18:54:17 +0300

    feature(calculate rate): 1. read new charged vocabulary 2. calculate article rate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import aiohttp
 import asyncio
+
+import aiohttp
+import pymorphy2
+
+import adapters
+from text_tools import calculate_jaundice_rate, split_by_words, read_charged_words
 
 
 async def fetch(session, url):
@@ -10,8 +15,18 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
-        print(html)
+        html = await fetch(session, 'https://inosmi.ru/20220908/polonez-255973070.html')
+
+    morph = pymorphy2.MorphAnalyzer()
+    sanitized_text = adapters.SANITIZERS['inosmi_ru'](html, plaintext=True)
+    charged_vocabulary = 'charged_dict'
+    word_paths = [f'{charged_vocabulary}/positive_words.txt', f'{charged_vocabulary}/negative_words.txt']
+
+    charged_words = read_charged_words(*word_paths)
+
+    article_words = split_by_words(morph, sanitized_text)
+
+    print(f'Рейтинг: {calculate_jaundice_rate(article_words, charged_words)}\nСлов в статье: {len(article_words)}')
 
 
 asyncio.run(main())
