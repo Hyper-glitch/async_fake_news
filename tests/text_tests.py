0a8db3b1f8f3db319dcce94142fd85d363c3e3b2
hyper-glitch <graff049@gmail.com>
2022-09-13 19:15:25 +0300

    refactor(move): mvoe tests in test module

--- tests/text_tests.py
+++ tests/text_tests.py
@@ -1,5 +1,9 @@
 import pymorphy2
+import pytest
+import requests
 
+from adapters import ArticleNotFound
+from adapters.inosmi_ru import sanitize
 from text_tools import calculate_jaundice_rate, split_by_words
 
 
@@ -16,3 +20,33 @@ def test_split_by_words():
     assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
 
     assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+
+
+def test_sanitize():
+    resp = requests.get('https://inosmi.ru/economic/20190629/245384784.html')
+    resp.raise_for_status()
+    clean_text = sanitize(resp.text)
+
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
+    assert 'За несколько часов до встречи с Си' in clean_text
+
+    assert '<img src="' in clean_text
+    assert '<h1>' in clean_text
+
+    clean_plaintext = sanitize(resp.text, plaintext=True)
+
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_plaintext
+    assert 'За несколько часов до встречи с Си' in clean_plaintext
+
+    assert '<img src="' not in clean_plaintext
+    assert '<a href="' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+    assert '</article>' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+
+
+def test_sanitize_wrong_url():
+    resp = requests.get('http://example.com')
+    resp.raise_for_status()
+    with pytest.raises(ArticleNotFound):
+        sanitize(resp.text)
